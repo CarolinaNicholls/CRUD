3691f1935553739aa189db3b97ea494ecb2b6bc5
Carolina <cnicholls@misena.edu.co>
2020-06-02 17:04:58 -0500

    cambios

---
+++ MVC/Vistas/modulos/editar.php
@@ -0,0 +1,22 @@
+
+<?php
+	session_start();
+	 if (!$_SESSION["Ingreso"]) {
+		header("location:index.php?ruta=ingreso");
+		exit();
+	 }
+?>
+	<br>
+	<h1>EDITAR EMPLEADOS</h1>
+
+	<form method="post" action="">
+
+
+        <?php
+        $editar = new EmpleadosC();
+		$editar -> EditarEmpleadoC();
+
+		$actualizar = new EmpleadosC();
+		$actualizar -> ActualizarEmpleadoC();
+?>
+	</form>
